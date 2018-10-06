9851fc760d7778fb57542cc321ccd827bf311093
Divyesh Chitroda <div5yesh@gmail.com>
2018-10-06 15:42:49 -0400

    the simple thing

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 document.body.onload = function () {
-	document.getElementById("menulist").addEventListener("click", onMenuClick);
+    // requestAnimationFrame(animate);
+
+    let t1 = document.getElementsByClassName("t1")[0],
+        t2 = document.getElementsByClassName("t2")[0],
+        centerpeice = document.getElementsByClassName("centerpeice")[0];
+
+    centerpeice.addEventListener("mouseover", function (event) {
+        centerpeice.classList.add("slide");
+    });
+
+    centerpeice.addEventListener("mouseout", function (event) {
+        centerpeice.classList.remove("slide");
+    });
+
+    // function animate() {
+    //     if (!start) start = time;
+    //     current = time - start;
+
+    //     if (current > 16) {
+
+    //     }
+
+    //     requestAnimationFrame(animate);
+    // }
 };
-
-function onMenuClick(event) {
-	var elem = event.target;
-
-	document.querySelectorAll("#menulist a").forEach(function (item) {
-		if (item.classList.contains("active")) {
-			item.classList.remove("active");
-		}
-	});
-
-	elem.classList.add("active");
-}
-
-function toggleHamburger(event) {
-	var elem = document.querySelector('.hamburger-content');
-    if (elem.style.display === "block") {
-        elem.style.display = "none";
-    } else {
-        elem.style.display = "block";
-    }
-}
