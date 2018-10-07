2d989b29c8bcec3efccf1625992afc3fa4d24f99
Divyesh Chitroda <div5yesh@gmail.com>
2018-10-06 20:49:20 -0400

    add about and projects

--- index.js
+++ index.js
@@ -3,16 +3,46 @@
 $(document).ready(function () {
 
     let navbar = $(".navbar");
+    let close = $(".close");
 
-    navbar.click(function (event) {
-        $(event.target).animate({
-            // "letter-spacing": "80px",
-        }, 500, "linear",
-            function () {
-            $(event.target).animate({
-                top: "-"+ parseInt($(event.target).attr("section")) * 25+"%",
-                height: "100%"
-            }, 1000, "linear");
-        });
+    let desg = $(".desg");
+
+    $(desg).animate(function () {
+
     });
+
+    $(navbar).click(function (event) {
+        if (event.target.classList.contains("navbar")) {
+            $(event.target).animate({
+                // "letter-spacing": "30px",
+            }, 500, "linear",
+                function () {
+                    $(event.target).animate({
+                        top: "-" + parseInt($(event.target).attr("section")) * 25 + "%",
+                        height: "100%"
+                    }, 1000, "linear", function () {
+
+                        });
+
+
+                        $(".close",event.target).animate({
+                            left: "20px"
+                        }, 500);
+                });
+        }
+    });
+
+    $(close).click(function (event) {
+
+        $(event.target).parent().animate({
+            height: "25%",
+            top: "",
+            // "letter-spcing": "15px"
+        }, 500);
+
+        $(close).animate({
+            left: "-50px"
+        }, 500)
+    });
+
 });
